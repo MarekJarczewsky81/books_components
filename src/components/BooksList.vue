1b17efa97f5f6356fb6e19de9223f8676a6e9055
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-06-09 20:03:11 +0200

    Update App books_components

--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -7,7 +7,7 @@
           {{ book.title }}, {{ book.price }}
           <!-- <button @click="removeBook(index)">Remove</button> -->
           <button @click="$emit('remove', index)">Remove</button>
-          <!-- przycisk emituje do rodzica event remove, przy okazji przekazując też informacje o indeksie książki do usunięcia -->
+          <!--funkcja $emit: przycisk emituje do rodzica event remove, przy okazji przekazując też informacje o indeksie książki do usunięcia -->
       </li>
     </ul>
     <p v-show="!books.length">No books...</p>
@@ -27,5 +27,10 @@ export default {
     //   required: true
     // }
   }
+  // Skrócona wersje zapisu zapisu, bez dodatkowego obiektu
+  // props: {
+  //   books: Array
+  // }
+  // props: ['books']
 }
 </script>
