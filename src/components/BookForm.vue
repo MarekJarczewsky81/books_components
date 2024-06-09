6754cdd0823ec5447a9e8d058d977ce0f44009ef
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-06-09 22:30:38 +0200

    Added BookForm component

---
+++ src/components/BookForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit.prevent="$emit('add', form), handleSubmit">
+      <label>
+        Title:
+        <input v-model="form.title" type="text" name="title">
+      </label>
+      <label>
+        price:
+        <input v-model="form.price" type="number" name="price">
+      </label>
+      <button>Add book</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Book Form',
+  data: () => ({
+    form: {
+      title: '',
+      price: 0
+    }
+  }),
+  methods: {
+    handleSubmit () {
+      this.$emit('add', this.form)
+    // here is your code for emptying the form
+    }
+  }
+}
+</script>
